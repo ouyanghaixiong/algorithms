e96498a193447bb53f288d14765fc43a02ed6ca0
ouyang <ouyanghaixiong@forchange.tech>
2021-05-12 22:29:41 +0800

    feat: simple ArrayList

---
+++ src/main/java/com/ouyanghaixiong/ArrayList.java
@@ -0,0 +1,96 @@
+package com.ouyanghaixiong;
+
+/**
+ * @author ouyanghaixiong@forchange.tech
+ * @date 2021/5/12
+ * @desc
+ */
+public class ArrayList {
+    private int[] arr;
+    private int capacity;
+    private int size;
+
+    public ArrayList(int capacity) {
+        this.arr = new int[capacity];
+        this.capacity = capacity;
+        this.size = 0;
+    }
+
+    public void insert(int index, int value) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("index < 0");
+        }
+
+        if (index > size) {
+            throw new IndexOutOfBoundsException("index > size");
+        }
+
+        if (size == capacity) {
+            capacity = 2 * capacity;
+            int[] newArr = new int[capacity];
+            System.arraycopy(arr, 0, newArr, 0, size);
+            arr = newArr;
+        }
+
+        if (size - index >= 0) {
+            System.arraycopy(arr, index, arr, index + 1, size - index);
+        }
+        arr[index] = value;
+        size++;
+    }
+
+    public void add(int value) {
+        insert(size,value);
+    }
+
+    public int remove(int index) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("index < 0");
+        }
+
+        if (index > size - 1) {
+            throw new IndexOutOfBoundsException("index > size - 1");
+        }
+
+        int removed = arr[index];
+        size--;
+
+        if (size - index >= 0) {
+            System.arraycopy(arr, index + 1, arr, index, size - index);
+        }
+
+        return removed;
+    }
+
+    public void set(int index, int value) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("index < 0");
+        }
+
+        if (index > size - 1) {
+            throw new IndexOutOfBoundsException("index > size");
+        }
+
+        arr[index] = value;
+    }
+
+    public int get(int index) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("index < 0");
+        }
+
+        if (index > size - 1) {
+            throw new IndexOutOfBoundsException("index > size - 1");
+        }
+
+        return arr[index];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
---
+++ src/test/java/com/ouyanghaixiong/ArrayListTest.java
@@ -0,0 +1,84 @@
+package com.ouyanghaixiong;
+
+import com.sun.xml.internal.ws.api.server.AbstractServerAsyncTransport;
+import oracle.jrockit.jfr.ActiveSettingEvent;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author ouyanghaixiong@forchange.tech
+ * @date 2021/5/12
+ * @desc
+ */
+public class ArrayListTest {
+    private ArrayList list;
+
+    @Before
+    public void setUp() throws Exception {
+        list = new ArrayList(3);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testInsertOutOfBounds() {
+        list.insert(1, 1);
+    }
+
+    @Test
+    public void testInsert() {
+        list.insert(0,0);
+        assertEquals(1, list.getSize());
+        list.insert(1,1);
+        assertEquals(2, list.getSize());
+        list.insert(1,1);
+        assertEquals(3, list.getSize());
+        list.insert(3, 3);
+        assertEquals(4, list.getSize());
+        assertEquals(6,list.getCapacity());
+    }
+
+    @Test
+    public void testAdd() {
+        list.add(10);
+        assertEquals(1, list.getSize());
+        assertEquals(3, list.getCapacity());
+        list.add(100);
+        list.add(1000);
+        list.add(10000);
+        list.add(100000);
+        assertEquals(5, list.getSize());
+        assertEquals(6, list.getCapacity());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveOutOfBounds() {
+        list.remove(0);
+    }
+
+    @Test
+    public void testRemove() {
+        list.add(0);
+        list.remove(0);
+        assertEquals(0, list.getSize());
+        list.add(1);
+        list.add(2);
+        int removed = list.remove(1);
+        assertEquals(1, list.getSize());
+        assertEquals(2, removed);
+    }
+
+    @Test
+    public void testSet() {
+        list.add(0);
+        list.set(0,1);
+        assertEquals(1,list.get(0));
+    }
+
+    @Test
+    public void testGet() {
+        list.add(0);
+        list.add(1);
+        assertEquals(1, list.get(1));
+    }
+}
