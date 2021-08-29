136450c8b14742bc13b523687b5d714b98eca1a0
ouyanghaixiong <ouyanghaixiong@forchange.tech>
2021-08-29 10:46:10 +0800

    feat: add CyclicQueue

---
+++ src/main/java/com/ouyanghaixiong/CyclicQueue.java
@@ -0,0 +1,44 @@
+package com.ouyanghaixiong;
+
+/**
+ * @author ouyanghaixiong@forchange.tech
+ * @date 2021/8/29
+ * @desc
+ */
+public class CyclicQueue {
+    private final int[] arr;
+    private final int capacity;
+    public int size;
+    private int front;
+    private int rear;
+
+    public CyclicQueue(int capacity) {
+        this.capacity = capacity;
+        this.arr = new int[capacity];
+        this.size = 0;
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    public void enQueue(int data) {
+        if ((rear + 1) % capacity == front) {
+            throw new RuntimeException("There is no space left in the queue.");
+        }
+
+        arr[rear] = data;
+        size++;
+        rear = (rear + 1) % capacity;
+    }
+
+    public int deQueue() {
+        if (front == rear) {
+            throw new RuntimeException("The queue is empty.");
+        }
+
+        int data = arr[front];
+        size--;
+        front = (front + 1) % capacity;
+
+        return data;
+    }
+}
---
+++ src/test/java/com/ouyanghaixiong/CyclicQueueTest.java
@@ -0,0 +1,35 @@
+package com.ouyanghaixiong;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author ouyanghaixiong@forchange.tech
+ * @date 2021/8/29
+ * @desc
+ */
+public class CyclicQueueTest {
+
+    @Test
+    public void enQueue() {
+        CyclicQueue queue = new CyclicQueue(10);
+        queue.enQueue(0);
+        queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+        assertEquals(4, queue.size);
+    }
+
+    @Test
+    public void deQueue() {
+        CyclicQueue queue = new CyclicQueue(10);
+        queue.enQueue(0);
+        queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+        queue.deQueue();
+        queue.deQueue();
+        assertEquals(2, queue.size);
+    }
+}
